818e139a8a41ec04bb6008c37f886a853384c98d
Guest User <Guest@epicodus-7.local>
2018-02-26 11:27:55 -0800

    add sort function to display posts in descending order based on likes

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,38 +5,37 @@ import Downvote from './Downvote';
 import PropTypes from 'prop-types';
 
 function PostList(props){
-  console.log(props.postList);
   const header =
     <div>
       <h5>Current Posts:</h5>
     </div>;
 
-  if (props.postList.keys) {
+  if (props.postList.length != 0) {
     return (
       <div>
         { header }
-        {Object.keys(props.postList).map(function(postId) {
-          var post  = props.postList[postId];
-          <div>
-            <div>
-              <Post
-                author={post.author}
-                content={post.content}
-                tags={post.tags}
-                timeStamp={post.timeStamp}
-                likes={post.likes}
-                key={post.id}
-                postId={post.id} />
-            </div>
-            <div className='row'>
-              <div className='col-sm-6'>
-                <Upvote post={post}/>
-              </div>
-              <div className='col-sm-6'>
-                <Downvote post={post}/>
-              </div>
-            </div>
-          </div>
+        {props.postList.map(function(postId, i) {
+          var post  = props.postList[i];
+          // code must started in the same line as return statement or the code must be wrapped in brackets
+          return (
+              <div>
+                  <Post
+                      author={post.author}
+                      content={post.content}
+                      tags={post.tags}
+                      timeStamp={post.timeStamp}
+                      likes={post.likes}
+                      key={post.id}
+                      postId={post.id} />
+                    <div className='row'>
+                      <div className='col-sm-6'>
+                        <Upvote post={post}/>
+                      </div>
+                      <div className='col-sm-6'>
+                        <Downvote post={post}/>
+                      </div>
+                    </div>
+                  </div>);
         })}
       </div>
     );
@@ -46,12 +45,12 @@ function PostList(props){
         { header }
         <p><em>There are currently no posts to display. Please fill out the form to the right to add a post.</em></p>
       </div>
-    )
+    );
   }
 }
 
 PostList.propTypes = {
-  postList: PropTypes.object
+  postList: PropTypes.array
 };
 
 
--- src/components/PostListContainer.jsx
+++ src/components/PostListContainer.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 function PostListContainer(props){
+
   return (
     <div>
       <PostList postList={props.masterPostList} />
@@ -12,14 +13,22 @@ function PostListContainer(props){
 }
 
 PostListContainer.propTypes = {
-  masterPostList: PropTypes.object,
+  masterPostList: PropTypes.array,
   dispatch: PropTypes.func
 };
 
 const mapStateToProps = state => {
-  return {
-    masterPostList: state
-  };
+  if (Object.keys(state).length != 0) {
+    let newState = Object.values(state);
+    let returnedList = newState.sort(function(a,b) {return b.likes - a.likes ;})
+    return {
+      masterPostList: returnedList
+    };
+  } else {
+    return {
+      masterPostList: []
+    };
+  }
 };
 
 export default connect(mapStateToProps)(PostListContainer);
